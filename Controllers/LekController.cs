a58e9603719505ca76d25c63bb843e53329d5f39
Michał Ostrowski <s20783@pjwstk.edu.pl>
2022-04-16 12:09:28 +0200

    Hashids i metody asynchroniczne

--- Controllers/LekController.cs
+++ Controllers/LekController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HashidsNet;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using PRO_API.DTO.Request;
 using PRO_API.Models;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -17,14 +17,22 @@ namespace PRO_API.Controllers
     {
         private readonly IConfiguration configuration;
         private readonly KlinikaContext context;
-        public LekController(IConfiguration config, KlinikaContext klinikaContext)
+        private readonly IHashids hashids;
+        public LekController(IConfiguration config, KlinikaContext klinikaContext, IHashids ihashids)
         {
             configuration = config;
             context = klinikaContext;
+            hashids = ihashids;
+        }
+
+        [HttpGet("hashid/{id}")]
+        public async Task<IActionResult> GetLekList(int id)
+        {
+            return Ok(hashids.Encode(1) + "  " + hashids.Encode(2));
         }
 
         [HttpGet]
-        public IActionResult GetLekList()
+        public async Task<IActionResult> GetLekList()
         {
             var query = "Select l.ID_lek, l.Nazwa, SUM(ilosc) as Ilosc, l.Jednostka_Miary from Lek l, Lek_W_Magazynie m " +
                 "where l.ID_lek = m.ID_lek AND Data_waznosci > GETDATE()" +
@@ -32,135 +40,176 @@ namespace PRO_API.Controllers
 
             DataTable table = new DataTable();
             SqlConnection connection = new SqlConnection(configuration.GetConnectionString("KlinikaDatabase"));
-            connection.Open();
-            
+            await connection.OpenAsync();
+
             SqlCommand command = new SqlCommand(query, connection);
-            SqlDataReader reader = command.ExecuteReader();
+            SqlDataReader reader = await command.ExecuteReaderAsync();
             table.Load(reader);
 
-            reader.Close();
-            connection.Close();
+            await reader.CloseAsync();
+            await connection.CloseAsync();
 
             return Ok(table);
         }
 
         [HttpGet("{ID_lek}")]
-        public IActionResult GetLekById(int ID_lek)
+        public async Task<IActionResult> GetLekById(string ID_lek)
         {
-            if (context.Leks.Where(x => x.IdLek == ID_lek).Any() != true)
+            var idArray = hashids.Decode(ID_lek);
+            if (idArray.Length == 0)
+            {
+                return NotFound();
+            }
+            int id = idArray[0];
+
+            if (context.Leks.Where(x => x.IdLek == id).Any() != true)
             {
                 return BadRequest("Nie ma leku o ID = " + ID_lek);
             }
-            else
-            {
-                var results =
-                from x in context.Leks
-                join y in context.LekWMagazynies on x.IdLek equals y.IdLek into ps
-                from p in ps
-                where x.IdLek == ID_lek
-                select new
-                {
-                    IdStanLeku = p.IdStanLeku,
-                    Nazwa = x.Nazwa,
-                    Jednostka_Miary = x.JednostkaMiary,
-                    Ilosc = p.Ilosc,
-                    Data_Waznosci = p.DataWaznosci
-                };
 
-                return Ok(results);
-            }
+            var results =
+            from x in context.Leks
+            join y in context.LekWMagazynies on x.IdLek equals y.IdLek into ps
+            from p in ps
+            where x.IdLek == id
+            select new
+            {
+                IdStanLeku = p.IdStanLeku,
+                Nazwa = x.Nazwa,
+                Jednostka_Miary = x.JednostkaMiary,
+                Ilosc = p.Ilosc,
+                Data_Waznosci = p.DataWaznosci
+            };
+
+            return Ok(results);
         }
 
         [HttpGet("{ID_lek}/{ID_stan_leku}")]
-        public IActionResult GetLekWMagazynieById(int ID_lek, int ID_stan_leku)
+        public IActionResult GetLekWMagazynieById(string ID_lek, string ID_stan_leku)
         {
-            if (context.Leks.Where(x => x.IdLek == ID_lek).Any() != true)
+            var idArray1 = hashids.Decode(ID_lek);
+            var idArray2 = hashids.Decode(ID_stan_leku);
+            if (idArray1.Length == 0 || idArray2.Length == 0)
+            {
+                return NotFound();
+            }
+            int id1 = idArray1[0];
+            int id2 = idArray2[0];
+
+            if (context.Leks.Where(x => x.IdLek == id1).Any() != true)
             {
                 return BadRequest("Nie ma leku o ID = " + ID_lek);
             }
-            else if(context.LekWMagazynies.Where(x => x.IdStanLeku == ID_stan_leku).Any() != true)
+
+            if (context.LekWMagazynies.Where(x => x.IdStanLeku == id2).Any() != true)
             {
                 return BadRequest("Nie ma leku o ID = " + ID_lek + " w magazynie");
             }
-            else
-            {
-                var results =
-                from x in context.Leks
-                join y in context.LekWMagazynies on x.IdLek equals y.IdLek into ps
-                from p in ps
-                where x.IdLek == ID_lek && p.IdStanLeku == ID_stan_leku
-                select new
-                {
-                    IdStanLeku = p.IdStanLeku,
-                    Nazwa = x.Nazwa,
-                    Jednostka_Miary = x.JednostkaMiary,
-                    Ilosc = p.Ilosc,
-                    Data_Waznosci = p.DataWaznosci
-                };
 
-                return Ok(results);
-            }
+            var results =
+            from x in context.Leks
+            join y in context.LekWMagazynies on x.IdLek equals y.IdLek into ps
+            from p in ps
+            where x.IdLek == id1 && p.IdStanLeku == id2
+            select new
+            {
+                IdStanLeku = p.IdStanLeku,
+                Nazwa = x.Nazwa,
+                Jednostka_Miary = x.JednostkaMiary,
+                Ilosc = p.Ilosc,
+                Data_Waznosci = p.DataWaznosci
+            };
+
+            return Ok(results);
         }
 
         [HttpPost("{ID_lek}")]
-        public IActionResult addStanLeku(int ID_lek, StanLekuRequest request)
+        public async Task<IActionResult> AddStanLeku(string ID_lek, StanLekuRequest request)
         {
+            var idArray = hashids.Decode(ID_lek);
+            if (idArray.Length == 0)
+            {
+                return NotFound();
+            }
+            int id = idArray[0];
+
             if (!ModelState.IsValid)
             {
                 return BadRequest("Niepoprawne dane");
             }
-            else if (!context.Leks.Where(x => x.IdLek == ID_lek).Any())
+
+            if (!context.Leks.Where(x => x.IdLek == id).Any())
             {
                 return BadRequest("Nie ma leku o ID = " + ID_lek);
             }
 
             context.LekWMagazynies.Add(new LekWMagazynie
             {
-                IdLek = ID_lek,
+                IdLek = id,
                 DataWaznosci = request.DataWaznosci,
                 Ilosc = request.Ilosc
             });
 
-            context.SaveChanges();
+            await context.SaveChangesAsync();
 
             return Ok("Dodano informacje o leku.");
         }
 
         [HttpPut("{ID_lek}/{ID_stan_leku}")]
-        public IActionResult updateStanLeku(int ID_lek, int ID_stan_leku, StanLekuRequest request)
+        public async Task<IActionResult> UpdateStanLeku(string ID_lek, string ID_stan_leku, StanLekuRequest request)
         {
+            var idArray1 = hashids.Decode(ID_lek);
+            var idArray2 = hashids.Decode(ID_stan_leku);
+            if (idArray1.Length == 0 || idArray2.Length == 0)
+            {
+                return NotFound();
+            }
+            int id1 = idArray1[0];
+            int id2 = idArray2[0];
+
             if (!ModelState.IsValid)
             {
                 return BadRequest("Niepoprawne dane");
             }
-            else if(!context.LekWMagazynies.Where(x => x.IdStanLeku == ID_stan_leku && x.IdLek == ID_lek).Any())
+
+            if (!context.LekWMagazynies.Where(x => x.IdStanLeku == id2 && x.IdLek == id1).Any())
             {
                 return BadRequest("Nie ma informacji o takim leku w magazynie.");
             }
 
-            var stanLeku = context.LekWMagazynies.Where(x => x.IdLek == ID_lek && x.IdStanLeku ==ID_stan_leku).First();
+            var stanLeku = context.LekWMagazynies.Where(x => x.IdLek == id1 && x.IdStanLeku == id2).First();
             stanLeku.Ilosc = request.Ilosc;
             stanLeku.DataWaznosci = request.DataWaznosci;
 
-            context.SaveChanges();
+            context.SaveChangesAsync();
 
             return Ok("Pomyślnie zaktuzalizowano dane.");
         }
 
         [HttpDelete("{ID_lek}/{ID_stan_leku}")]
-        public IActionResult deleteStanLeku(int ID_lek, int ID_stan_leku)
+        public async Task<IActionResult> DeleteStanLeku(string ID_lek, string ID_stan_leku)
         {
+            var idArray1 = hashids.Decode(ID_lek);
+            var idArray2 = hashids.Decode(ID_stan_leku);
+            if (idArray1.Length == 0 || idArray2.Length == 0)
+            {
+                return NotFound();
+            }
+            int id1 = idArray1[0];
+            int id2 = idArray2[0];
+
             if (!ModelState.IsValid)
             {
                 return BadRequest("Niepoprawne dane");
             }
-            else if(!context.LekWMagazynies.Where(x => x.IdStanLeku == ID_stan_leku && x.IdLek == ID_lek).Any())
+
+            if (!context.LekWMagazynies.Where(x => x.IdStanLeku == id2 && x.IdLek == id1).Any())
             {
                 return BadRequest("Nie ma informacji o takim leku w magazynie.");
             }
 
-            context.Remove(context.LekWMagazynies.Where(x => x.IdStanLeku == ID_stan_leku && x.IdLek == ID_lek).First());
-            context.SaveChanges();
+            context.Remove(context.LekWMagazynies.Where(x => x.IdStanLeku == id2 && x.IdLek == id1).First());
+            context.SaveChangesAsync();
 
             return Ok("Pomyślnie usunięto informacje o leku.");
         }
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using HashidsNet;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,6 +36,8 @@ namespace PRO_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHashids>(x => new Hashids(Configuration["HashidsSecret"], 5));
+
             services.AddAuthentication(opt =>
             {
                 opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;

392993b21c275e7bb635c190d54e89df8521c74f
Michał Ostrowski <s20783@pjwstk.edu.pl>
2022-04-20 19:36:24 +0200

    dodanie warstwy Domain

---
+++ Domain/Entities/Klient.cs
@@ -0,0 +1,27 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Klient
+    {
+        public int IdOsoba { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+        public string NumerTelefonu { get; set; }
+        public string Email { get; set; }
+        
+        public string Haslo { get; set; }
+        public string Salt { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime? RefreshTokenExp { get; set; }
+        public DateTime DataZalozeniaKonta { get; set; }
+
+        public Account account { get; }
+    }
+}
